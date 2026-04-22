420a3d3803f1942071205cb2edc17d0397b4d7fc
TomasFranchinoDev <tomasagustinfranchino@alumnos.ucse.edu.ar>
2026-04-22 19:56:44 -0300

    5commit

--- config/api.py
+++ config/api.py
@@ -5,8 +5,11 @@ from catalog.api import router as catalog_router
 from orders.api import router as orders_router
 from users.api import router as users_router
 
-api = NinjaAPI(title="Fenix B2B API", version="1.0.0")
-
+api = NinjaAPI(title="Fenix API", version="1.0.0")
+# Health check
+@api.get("/health", tags=["health"])
+def health(request):
+    return {"status": "ok"}
 api.add_router("/catalog", catalog_router)
 api.add_router("/orders", orders_router)
 api.add_router("/users/", users_router)
